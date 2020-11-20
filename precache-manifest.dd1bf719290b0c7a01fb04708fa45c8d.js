07c3bb686a2d077b27dea8862392baf08de14790
SameerMohdMomin <sameer.momin@somaiya.edu>
2020-11-20 19:32:20 +0530

    Updates

---
+++ precache-manifest.dd1bf719290b0c7a01fb04708fa45c8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d9d71fb24ec7ef3fba38fe500dfcd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48ec9e1df3ce072cf504",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85ad4ef0b853da0f997f",
+    "url": "/static/js/2.07d6039f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.07d6039f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ec9e1df3ce072cf504",
+    "url": "/static/js/main.da6773ee.chunk.js"
+  },
+  {
+    "revision": "38af698b43c981c0fe55",
+    "url": "/static/js/runtime-main.2d57e030.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
